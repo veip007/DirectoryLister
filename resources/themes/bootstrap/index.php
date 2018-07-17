b620c51ba40c67a413f168501e5beccffc064836
Toyo <toyo13140010096@gmail.com>
2018-07-17 21:50:11 +0800

    Update index.php

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -68,7 +68,9 @@ if(file_exists($md_file)){
         </div>
 		<div class="container">
         <div class="announcement">
+		<!-- 顶部公告栏 -->
 		<i class="fa fa-volume-down" style="display: inline-block;font-size: 15px;margin: 0;line-height: 1;color: #444;"></i><p style="display: inline-block;font-size: 15px;margin: 0;line-height: 1;text-indent: 5px;" >顶部公告栏</p>
+		<!-- 顶部公告栏 -->
 		</div>
 		</div>
 		<div id="page-content" class="container">
