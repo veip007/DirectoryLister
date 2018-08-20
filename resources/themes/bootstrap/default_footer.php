8ca2258c440192b9cda823296ae2aca7e0302c11
Toyo <toyo13140010096@gmail.com>
2018-08-20 14:04:41 +0800

    Update default_footer.php

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,3 +1,5 @@
-<div class="footer">
-    <a href="https://doub.io/dbrj-3/" target="_blank">逗比魔改版</a> / <a href="https://github.com/ToyoDAdoubi/DirectoryLister" target="_blank">Github开源</a> · 基于 <a href="http://www.directorylister.com/" target="_blank">Directory Lister</a> © 2015-<?php echo date("Y")?>
-</div>
+<footer class="container">
+  <div class="footer">
+ © 2015-<?php echo date("Y")?> <a href="https://doub.io" target="_blank">逗比根据地</a> , Inc.
+   </div>
+</footer>
