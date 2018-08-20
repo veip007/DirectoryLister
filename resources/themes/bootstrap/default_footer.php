5000c9552595fd6004ea6d5c4564b4b57d90f3c1
Toyo <toyo13140010096@gmail.com>
2018-08-20 16:33:23 +0800

    Update default_footer.php

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,5 +1,5 @@
 <footer class="container">
   <div class="footer">
- © 2015-<?php echo date("Y")?> <a href="https://doub.io" target="_blank">逗比根据地</a> , Inc.
+ © 2015-<?php echo date("Y")?> <a href="https://doub.io" target="_blank">逗比根据地</a> . All rights reserved.
    </div>
 </footer>
